08e4f8b65cc6e865b3761f1711c53dece521d8d6
bati668 <e1106117@gmail.com>
2017-04-16 00:16:11 +0900

    Initial Commit

---
+++ Skyway_swift_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skyway_swift_sample
+//
+//  Created by 千葉大志 on 2017/04/16.
+//  Copyright © 2017年 bati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
